486660ca23bb13cd26c9fa12bc0a6a9fd51488d0
Arthur Xavier <arthur.xavierx@gmail.com>
2016-09-13 17:56:21 -0300

    docs(Carpenter.Cedar): add CounterList example using new `watch` function

---
+++ examples/CounterList/Maybe/Counter.purs
@@ -0,0 +1,35 @@
+module Examples.CounterList.Maybe.Counter where
+
+import Prelude
+import Carpenter (Render, Update)
+import Carpenter.Cedar (CedarClass, cedarSpec)
+import Data.Maybe (Maybe(..))
+import React (createClass)
+import React.DOM (button, div', text, span')
+import React.DOM.Props (onClick)
+
+type Counter = Maybe Int
+
+data CounterAction = Increment | Decrement | Remove
+
+counter :: CedarClass Counter CounterAction
+counter = createClass $ cedarSpec update render
+
+update :: forall props eff. Update Counter props CounterAction eff
+update yield action _ _ =
+  case action of
+    Increment ->
+      yield $ map (_ + 1)
+    Decrement ->
+      yield $ map (_ - 1)
+    Remove ->
+      yield $ const Nothing
+
+render :: forall props. Render Counter props CounterAction
+render dispatch _ state _ =
+  div'
+    [ span' [text (show state)]
+    , button [onClick \_ -> dispatch Increment] [text "+"]
+    , button [onClick \_ -> dispatch Decrement] [text "-"]
+    , button [onClick \_ -> dispatch Remove] [text "X"]
+    ]
