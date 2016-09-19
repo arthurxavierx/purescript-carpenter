ede65c30f1f1783afe424c90196b462087339c99
Arthur Xavier <arthur.xavierx@gmail.com>
2016-09-19 20:58:12 -0300

    docs(examples): refactor 'Maybe' example to use the Elm architecture

---
+++ examples/CounterListElm/Counter.purs
@@ -0,0 +1,30 @@
+module Examples.CounterListElm.Counter where
+
+import Prelude
+import Carpenter (Render)
+import Data.Maybe (Maybe(..))
+import React.DOM (button, div', text, h1')
+import React.DOM.Props (onClick)
+
+type Counter = Int
+
+data CounterAction = Increment | Decrement | Remove
+
+update :: CounterAction -> Counter -> Maybe Counter
+update action state =
+  case action of
+    Increment ->
+      Just (state + 1)
+    Decrement ->
+      Just (state - 1)
+    Remove ->
+      Nothing
+
+render :: forall props. Render Counter props CounterAction
+render dispatch _ state _ =
+  div'
+    [ h1' [text (show state)]
+    , button [onClick \_ -> dispatch Increment] [text "+"]
+    , button [onClick \_ -> dispatch Decrement] [text "-"]
+    , button [onClick \_ -> dispatch Remove] [text "X"]
+    ]
---
+++ examples/CounterListElm/CounterList.purs
@@ -0,0 +1,39 @@
+module Examples.CounterListElm.CounterList where
+
+import Prelude
+import Examples.CounterListElm.Counter as Counter
+import Carpenter (Render, Update, spec)
+import Data.Array (mapWithIndex, mapMaybe, snoc)
+import Data.Maybe (Maybe(Just))
+import Examples.CounterListElm.Counter (CounterAction, Counter)
+import React (ReactClass, createClass)
+import React.DOM (text, button, div')
+import React.DOM.Props (onClick)
+
+type CounterList = Array Counter
+
+data CounterListAction = Add | UpdateCounter Int CounterAction
+
+counterListClass :: ReactClass _
+counterListClass = createClass $ spec [0] update render
+
+update :: forall props eff. Update CounterList props CounterListAction eff
+update yield _ action _ _ =
+  case action of
+    Add ->
+      yield \state -> snoc state 0
+
+    UpdateCounter i caction -> do
+      yield $ mapMaybe id <<< mapWithIndex (updateCounter i caction)
+
+  where
+    updateCounter id caction i counter
+      | id == i   = Counter.update caction counter
+      | otherwise = Just counter
+
+render :: forall props. Render CounterList props CounterListAction
+render dispatch _ state _ =
+  div'
+    [ div' $ mapWithIndex (\i c -> Counter.render (dispatch <<< UpdateCounter i) {} c []) state
+    , button [onClick \_ -> dispatch Add] [text "++"]
+    ]
